10ee3429c1b1bb3143eaeb1b10bc49fd53273c21
Lionel <lionel@loveissue.local>
2020-11-06 19:51:01 +0100

    online

--- pages/api/portfolio.js
+++ pages/api/portfolio.js
@@ -1,17 +1,14 @@
-import { PrismaClient } from "@prisma/client"
+// import { PrismaClient } from "@prisma/client"
 
-const prisma = new PrismaClient()
+// const prisma = new PrismaClient()
 
-export default async (req, res) => {
-  try {
-    await prisma.$connect
-    const clients = await prisma.portfolio.findMany()
-    res.status(200)
-    res.json({ clients })
-  } catch (e) {
-    res.status(500)
-    res.json({ error: "Unable to fetch portfolio" })
-  } finally {
-    await prisma.$disconnect()
-  }
-}
+// export default async (req, res) => {
+//   try {
+//     const clients = await prisma.portfolio.findMany()
+//     res.status(200)
+//     res.json({ clients })
+//   } catch (e) {
+//     res.status(500)
+//     res.json({ error: "Unable to fetch portfolio" })
+//   }
+// }
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,8 +1,7 @@
 import Layout from "../components/Layout"
 import Head from "next/head"
 
-const portfolio = ({ data }) => {
-  console.log(data)
+const portfolio = () => {
   return (
     <Layout>
       <Head>
@@ -12,17 +11,18 @@ const portfolio = ({ data }) => {
         <h1>PORTFOLIO</h1>
         <h3>LIONEL LORD</h3>
       </div>
+      <h1 className="portfolio">Coming Soon</h1>
     </Layout>
   )
 }
 
-export async function getServerSideProps() {
-  // Fetch data from external API
-  const res = await fetch(`http://lionellord.nl/api/portfolio`)
-  const data = await res.json()
+// export async function getServerSideProps() {
+//   // Fetch data from external API
+//   const res = await fetch(`http://localhost:3000/api/portfolio`)
+//   const data = await res.json()
 
-  // Pass data to the page via props
-  return { props: { data } }
-}
+//   // Pass data to the page via props
+//   return { props: { data } }
+// }
 
 export default portfolio
